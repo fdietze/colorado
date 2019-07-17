c4048469925e65674de5d8bbb8f106e42cbcc565
Scala Steward <me@scala-steward.org>
2019-07-17 22:21:14 +0200

    Update scalatest to 3.0.8

--- project/Deps.scala
+++ project/Deps.scala
@@ -6,7 +6,7 @@ object Deps {
   // hack to expand %%% in settings, needs .value in build.sbt
   import Def.{setting => dep}
 
-  val scalaTest = dep("org.scalatest" %%% "scalatest" % "3.0.4")
+  val scalaTest = dep("org.scalatest" %%% "scalatest" % "3.0.8")
   val cats = new {
     val core = dep("org.typelevel" %%% "cats-core" % "1.0.1")
   }
